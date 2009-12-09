0042bdc45d057b31e17d95698083e2e28053674c
mfujisa <mfujisa@d94ef9e2-4a8f-11de-a3b0-c1e83b3b5a46>
2009-12-09 09:35:57 +0000

    [trunk] modify comment of plsi

--- trunk/plsi.cc
+++ trunk/plsi.cc
@@ -16,7 +16,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-// I refer the following pLSI package written by Taku Kudo.
+// I referred the following pLSI package written by Taku Kudo.
 // http://chasen.org/~taku/software/plsi/
 //
 
